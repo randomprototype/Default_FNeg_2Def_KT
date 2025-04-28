2410af9a6c22bb20727a7126683bfd7b8818714d
randomprototype <100228373+randomprototype@users.noreply.github.com>
2025-04-28 14:29:04 -0300

    Update MainCode.py

--- MainCode.py
+++ MainCode.py
@@ -3,7 +3,7 @@ import numpy as np
 import sys
 from streamlit import cli as stcli
 from scipy.integrate import quad #Single integral
-from scipy.integrate import dblquad
+from scipy.integrate import dblquad, tplquad
 from PIL import Image
 
 ###Defining the p.d.f.s and reliability functions############################
@@ -233,7 +233,7 @@ def main():
     #inserindo na coluna 2
     col2.image(foto, use_column_width=True)
     #O código abaixo centraliza e atribui cor
-    st.markdown("<h2 style='text-align: center; color: #306754;'>HIDF-2D: Hybrid Inspection/Age-based with Defaults, False-negative and 2 levels of Defect</h2>", unsafe_allow_html=True)
+    st.markdown("<h2 style='text-align: center; color: #306754;'>HIDFI-2D: Hybrid Inspection/Age-based with Defaults, False-negative, Defect Induction and 2 levels of Defect Policy</h2>", unsafe_allow_html=True)
     
     st.markdown("""
         <div style="background-color: #F3F3F3; padding: 10px; text-align: center;">
@@ -250,55 +250,49 @@ def main():
         st.header(menu[0])
         st.subheader("Insert the parameter values below:")
         
-        global a2,b2,a1,b1,p,l,alfa,beta,ci,cr,cf,cd
-        a2=st.number_input("Insert the scale parameter for the defect arrival distribution of “strong” components (η\u2081)", min_value = 0.0, value = 3.0, help="This parameter specifies the scale parameter for the Weibull distribution, representing the defect arrival for the stronger component.")
-        b2=st.number_input("Insert the shape parameter for the defect arrival distribution of “strong” components (β\u2082)", min_value = 1.0, max_value=5.0, value = 2.5, help="This parameter specifies the shape parameter for the Weibull distribution, representing the defect arrival for the stronger component.")
-        a1=st.number_input("Insert the scale parameter for the defect arrival distribution of “weak” components (η\u2081)", min_value = 3.0, value = 18.0, help="This parameter specifies the scale parameter for the Weibull distribution, representing the defect arrival for the weaker component.")
-        b1=st.number_input("Insert the shape parameter for the defect arrival distribution of “weak” components (β\u2082)", min_value = 1.0, max_value=5.0, value = 5.0, help="This parameter specifies the shape parameter for the Weibull distribution, representing the defect arrival for the weaker component.")
-        p=st.number_input("Insert the mixture parameter (p)", min_value = 0.0, max_value=1.0, value = 0.10, help="This parameter indicates the proportion of the weaker component within the total population of components.")
-        l=st.number_input("Insert the rate of the exponential distribution for delay-time (λ)", min_value = 0.0, value = 2.0, help="This parameter defines the rate of the Exponential distribution, which governs the transition from the defective to the failed state of a component.")
-        alfa=st.number_input("Insert the false-positive probability (\u03B1)", min_value = 0.0, max_value=1.0, value = 0.1, help="This parameter represents the probability of indicating a defect during inspection when, in fact, it does not exist.")
-        beta=st.number_input("Insert the false-negative probability (\u03B5)", min_value = 0.0, max_value=1.0, value = 0.15, help="This parameter represents the probability of not indicating a defect during inspection when, in fact, it does exist.")
-        ci=st.number_input("Insert cost of inspection (C_{I})", min_value = 0.0, value = 0.05, help="This parameter represents the cost of conducing an inspection.")
-        cr=st.number_input("Insert cost of replacement (inspections and age-based) (C_{R})", min_value = 0.0, value = 1.0, help="This parameter represents the cost associated with preventive replacements, whether performed during inspections or when the age-based threshold is reached.")
-        cf=st.number_input("Insert cost of failure (C_{F})", min_value = 0.0, value = 10.0, help="This parameter represents the replacement cost incurred when a component fails.")
-        cd=st.number_input("Insert cost of defective by time unit (C_{D})", min_value = 0.0, value = 0.01, help="This parameter represents the unitary cost associated with the time in which the component stays in defective state for each time unit.")
+        global a1,b1,a2,b2,a3,b3,Ci,Cr,Cf,Cmd,beta,q,p
+        a1=st.number_input("Insert the scale parameter for the minor defect arrival distribution (η_{X})", min_value = 0.0, value = 3.0, help="This parameter specifies the scale parameter for the Weibull distribution, representing the minor defect arrival.")
+        b2=st.number_input("Insert the shape parameter for the minor defect arrival distribution (κ_{X})", min_value = 1.0, max_value=5.0, value = 2.5, help="This parameter specifies the shape parameter for the Weibull distribution, representing the minor defect arrival.")
+        a2=st.number_input("Insert the scale parameter for the major defect arrival distribution (η_{Y})", min_value = 0.0, value = 5.0, help="This parameter specifies the scale parameter for the Weibull distribution, representing the time from minor to major defect.")
+        b2=st.number_input("Insert the shape parameter for the major defect arrival distribution (κ_{Y})", min_value = 1.0, max_value=5.0, value = 5.0, help="This parameter specifies the shape parameter for the Weibull distribution, representing the time from minor to major defect.")
+        a3=st.number_input("Insert the scale parameter for the failure arrival distribution (η_{H})", min_value = 0.0, value = 5.0, help="This parameter specifies the scale parameter for the Weibull distribution, representing the time from major defect to failure.")
+        b3=st.number_input("Insert the shape parameter for the failure arrival distribution (κ_{H})", min_value = 1.0, max_value=5.0, value = 5.0, help="This parameter specifies the shape parameter for the Weibull distribution, representing the time from major defect to failure.")
+        Ci=st.number_input("Insert cost of inspection (C_{I})", min_value = 0.0, value = 0.05, help="This parameter represents the cost of conducing an inspection.")
+        Cr=st.number_input("Insert cost of replacement (inspections and age-based) (C_{r})", min_value = 0.0, value = 1.0, help="This parameter represents the cost associated with preventive replacements, whether performed during inspections or when the age-based threshold is reached.")
+        Cf=st.number_input("Insert cost of failure (C_{f})", min_value = 0.0, value = 10.0, help="This parameter represents the replacement cost incurred when a component fails.")
+        Cmd=st.number_input("Insert cost of defective by time unit (C_{md})", min_value = 0.0, value = 0.01, help="This parameter represents the unitary cost associated with the time in which the component stays in defective state for each time unit.")
+        beta=st.number_input("Insert the false-negative probability (β)", min_value = 0.0, max_value=1.0, value = 0.15, help="This parameter represents the probability of not indicating a defect during inspection when, in fact, it does exist.")
+        q=st.number_input("Insert the probability of inducing minor defect in inspections (q)", min_value = 0.0, max_value=1.0, value = 0.15, help="This parameter represents the probability of not indicating a defect during inspection when, in fact, it does exist.")
+        p=st.number_input("Insert the probability of default in inspections (p)", min_value = 0.0, max_value=1.0, value = 0.15, help="This parameter represents the probability of not indicating a defect during inspection when, in fact, it does exist.")
         
         col1, col2 = st.columns(2)
         
-        Delta=[0]
         st.subheader("Insert the variable values below:")
         K=int(st.text_input("Insert the number of inspections (K)", value=4))
         if (K<0):
             K=0
         Value=2
-        if (K>0):
-            for i, col in enumerate(st.columns(K)):
-                col.write(f"**{i+1}-th inspection:**")
-                Delta.append(col.number_input("Insp. Mom. (Δ)", value=Value*(i+1), min_value=Delta[i-1], key=f"Delta_{i}"))
-        T = st.number_input("Insert the moment for the age-based preventive action (T)",value=(K+1)*Value,min_value=Delta[-1])
+        T = st.number_input("Insert the constant interval between sucessive inspections (T)", value=1.0,min_value=0.00001)
         
         st.subheader("Click on botton below to run this application:")    
         botao = st.button("Get cost-rate")
         if botao:
             st.write("---RESULT---")
-            st.write("Cost-rate", KD_KT(K, Delta, T))
+            st.write("Cost-rate", taxa_de_custo(K, T))
          
     if choice == menu[1]:
         st.header(menu[1])
-        st.write("<h6 style='text-align: justify; color: Blue Jay;'>This app is dedicated to compute the cost-rate for a hybrid aperiodic inspection and age-based maintenance policy. We assume a single system operating under Delay-Time Modeling (DTM) with a heterogeneous component lifetime, each having distinct defect arrival distributions. Component renovation occurs either after a failure (corrective maintenance) or during inspections, once a defect is detected or if the age-based threshold is reached (preventive maintenance). We considered false-positive and false-negative probabilities during the inspection.</h6>", unsafe_allow_html=True)
-        st.write("<h6 style='text-align: justify; color: Blue Jay;'>The app computes the cost-rate for a specific solution—defined by the number of inspections (K), inspection moments (Δ) and the age-based threshold (T).</h6>", unsafe_allow_html=True)
+        st.write("<h6 style='text-align: justify; color: Blue Jay;'>This app is dedicated to compute the cost-rate for a hybrid periodic inspection and age-based maintenance policy. We assume a single system operating under a three-stage failure process. Component renovation occurs either after a failure (corrective maintenance) or during inspections, once a defect is detected or if the age-based threshold is reached (preventive maintenance). We considered false-negative probabilities during inspections for minor defect detections, probability of inducing minor defects due to bad inspections and defaults in inspections.</h6>", unsafe_allow_html=True)
+        st.write("<h6 style='text-align: justify; color: Blue Jay;'>The app computes the cost-rate for a specific solution—defined by the number of inspections (K) and constant interval between sucessive inspections. At the moment (K+1)*T, then the age-based action is conduced.</h6>", unsafe_allow_html=True)
         st.write("<h6 style='text-align: justify; color: Blue Jay;'>For further questions or information on finding the optimal solution, please contact one of the email addresses below.</h6>", unsafe_allow_html=True)
         
         st.write('''
 
-v.h.r.lima@random.org.br
-
 r.g.n.paiva@random.org.br
 
-a.j.s.rodrigues@random.org.br
+v.h.r.lima@random.org.br
 
-h.s.j.gonzalez@random.org.br
+a.j.s.rodrigues@random.org.br
 
 c.a.v.cavalcante@random.org.br
 
